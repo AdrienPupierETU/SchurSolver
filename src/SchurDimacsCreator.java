f337a6b2f1bbbf8500d26702399ac8a824c4572a
eckterolite <apupier3@gmail.com>
2020-03-10 21:10:51 +0100

    seems to work, need testing

--- src/SchurDimacsCreator.java
+++ src/SchurDimacsCreator.java
@@ -1,5 +1,5 @@
 public class SchurDimacsCreator {
-    public static String createDimacs(int n, int k){
+    public  String createDimacs(int n, int k){
         int nbVar=n*k; //n*k
         int nbClause=0;
         StringBuilder stringBuilder= new StringBuilder();
@@ -9,10 +9,10 @@ public class SchurDimacsCreator {
         for(int i=0;i<n;i++){ //boule i
             StringBuilder clauseLoc= new StringBuilder();
             for(int j=0;j<k;j++){ //boite j
-                clauseLoc.append(varcount+" ");
+                clauseLoc.append(varcount).append(" ");
                 varcount++;
             }
-            clauseLoc.append(0+"\n");
+            clauseLoc.append(0).append("\n");
             stringBuilder.append(clauseLoc.toString());
             nbClause++;
         }
@@ -20,25 +20,44 @@ public class SchurDimacsCreator {
         for(int i=1;i<=nbVar;i++){
             for(int j=1;j<k;j++){
                 StringBuilder clauseLoc= new StringBuilder();
-                double res1=(double)i/k;
-                double res2=(double)(i+j)/k;
-                //ajouter if si res1 et res2 ne sont pas entre les mêmes entiers alors break;
-                if(res1==Math.floor(res1)){
+                int res1=(int)Math.ceil((double)i/k); // valeur de la boule de depart
+                int res2=(int)Math.ceil((double)(i+j)/k); // valeur de la boule plus le nombre de boite que l'on parcours
+                if(res1!=res2){  // si ce n'est pas la même boule.
                     break;
                 }
-                if(Math.floor(res1)!=Math.floor(res2-0.000001)){
-                    break;
-                }
-                clauseLoc.append(-i+" "+-(i+j)+" ");
-                clauseLoc.append(0+"\n");
+                clauseLoc.append(-i).append(" ").append(-(i + j)).append(" ");
+                clauseLoc.append(0).append("\n");
                 stringBuilder.append(clauseLoc.toString());
                 nbClause++;
             }
 
         }
         //x+y=z interdit dans une boite
+        for(int i=1;i<=nbVar;i++){
+            for(int j=1;j<=nbVar;j++){
+                int x=(int)Math.ceil((double)i/k);
+                int y=(int)Math.ceil((double)j/k);
+                if(x==y){
+                    continue;
+                }
+                int z=x+y;
 
+                if(z<=n){
+                    for(int l=0;l<k;l++){ // bannir cette combinaison dans chaque boite
+                        StringBuilder clauseLoc = new StringBuilder();
+                        int xn=(x*k)-(k-1); //boule x dans boite 1
+                        int yn=(y*k)-(k-1); //boule y dans boite 1
+                        int zn=(z*k)-(k-1); //boule z dans boite 1
+                        clauseLoc.append(-(xn+l)).append(" ").append(-(yn+l)).append(" ");
 
+                        clauseLoc.append(-(zn+l)).append(" ");
+                        clauseLoc.append(0).append("\n");
+                        stringBuilder.append(clauseLoc.toString()); // on ajoute a la chaine principale
+                        nbClause++;
+                    }
+                }
+            }
+        }
         stringBuilder.insert(0,"p cnf "+nbVar +" "+nbClause+" \n");
         return stringBuilder.toString();
     }
--- src/app.java
+++ src/app.java
@@ -1,6 +1,18 @@
+import java.sql.PseudoColumnUsage;
+
 public class app {
     public static void main(String[] args){
-        String result = SchurDimacsCreator.createDimacs(20,3);
+        if(args.length<2){
+            usage();
+            return;
+        }
+        int n=Integer.parseInt(args[0]);
+        int k=Integer.parseInt(args[1]);
+        SchurDimacsCreator creator = new SchurDimacsCreator();
+        String result = creator.createDimacs(n,k);
         System.out.println(result);
+    }
+    static void usage(){
+        System.err.println("Usage : java -jar SchurDimacsCreator n k ");
     }
 }
